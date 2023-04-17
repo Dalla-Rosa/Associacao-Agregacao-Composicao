bc01fab11450afe70aff5a9fb4548d41485a9510
Bernardo Flores Dalla Rosa <88604573+Dalla-Rosa@users.noreply.github.com>
2023-04-17 15:47:00 -0300

    Create Pedido.java

---
+++ Pedido.java
@@ -0,0 +1,49 @@
+package com.mycompany.pedido;
+import java.util.Scanner;
+
+
+public class Pedido {
+public int itemPedido;
+public double valorTotal;
+        
+
+
+public void adicionarItem(itemPedido it){
+    
+
+
+}
+
+public void obterTotal(){
+
+
+
+
+}
+
+    public int getItemPedido() {
+        return itemPedido;
+    }
+
+    public void setItemPedido(int itemPedido) {
+        this.itemPedido = itemPedido;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+    }
+
+}
