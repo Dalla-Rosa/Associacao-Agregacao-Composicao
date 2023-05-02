e53484aa63cf6d834f446564c478aab7150dfc67
Bernardo Flores Dalla Rosa <88604573+Dalla-Rosa@users.noreply.github.com>
2023-05-02 16:11:10 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,6 +1,7 @@
 package produto;
 
 public class Produto {
+    private String nome;
     private int codigo;
     private double valor;
     public String desc;
@@ -10,5 +11,29 @@ public class Produto {
         this.valor = valor;
         this.desc = desc;
     }
+    
+    public double getValor(){
+        return valor;
+    }
+    
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+    
+    public int getCodigo(){
+        return codigo;
+    }
+    
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
 }
-
